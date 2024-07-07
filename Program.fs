2bf7e966d9f833c5af01492d74ef2a5e4270b50f
k-ichirof <abachoppa1892@gmail.com>
2024-07-07 13:11:41 +0900

    ping確認

--- Program.fs
+++ Program.fs
@@ -1,2 +1,23 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿namespace CleanArchitecture
+
+module Program =
+    open Microsoft.AspNetCore.Builder
+    open System
+    open Microsoft.AspNetCore.Http
+
+    let exitCode = 0
+
+    type PingResponse = { pong: bool }
+
+    [<EntryPoint>]
+    let main args =
+        let builder = WebApplication.CreateBuilder(args)
+        let app = builder.Build()
+
+
+        let ping () : IResult = Results.Ok { pong = false }
+
+        app.MapGet("/ping", Func<IResult> ping)
+
+        app.Run()
+        exitCode
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
+# create project
 - dotnet new console -lang="F#" -o="CleanArchitecture"
+
+# ping確認
+- dotnet add package Microsoft.AspNetCore
+- dotnet add package Microsoft.AspNetCore.App
+- dotnet watch
+- curl -v http://localhost:5000/pin
